e98a288954834f4267dafa0355e87583de593347
Adam Ranfelt <adamRenny@gmail.com>
2014-10-06 15:27:50 -0500

    * Transferred doT templates to handlebars templates  * Updated renderer to use handlebars

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,4 @@
-var dotEmc = require('dot-emc');
+var expressHandlebars = require('express-handlebars');
 var path = require('path');
 var server = require('./server');
 var CONFIG = require('./config');
@@ -6,18 +6,23 @@ var CONFIG = require('./config');
 function Renderer() {}
 
 function setUp() {
+    this.impl = expressHandlebars.create({
+        extname: '.hbs',
+        defaultLayout: 'core',
+        layoutsDir: path.join(CONFIG.ROOT, 'src', 'view', 'layout'),
+        partialsDir: path.join(CONFIG.ROOT, 'src', 'view', 'partial')
+    });
+
     var impl = server.impl;
-    impl.set('views', path.join(CONFIG.ROOT, 'src', 'views'));
-    impl.set('view engine', 'html');
-    impl.engine('html', dotEmc.init({
-        app: impl,
-        fileExtension: 'html',
-        options: {
-            templateSettings: {
-                cache: CONFIG.IS_PRODUCTION
-            }
-        }
-    }).__express);
+
+    impl.set('views', path.join(CONFIG.ROOT, 'src', 'view'));
+
+    impl.set('view engine', 'hbs');
+    impl.engine('hbs', this.impl.engine);
+
+    if (CONFIG.IS_PRODUCTION) {
+        impl.enable('view cache');
+    }
 
     impl.set('baseUrl', CONFIG.BASE_URL);
 }
